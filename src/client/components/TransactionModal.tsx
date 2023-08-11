94827c9fb4c03bba41281bb4c4cfbe75618b56f8
Anthony Romaine <61034923+anthonyromaine@users.noreply.github.com>
2023-08-11 11:42:25 -0600

    Add form to transaction modal

--- src/client/components/TransactionModal.tsx
+++ src/client/components/TransactionModal.tsx
@@ -1,29 +1,45 @@
 import React, { useState } from 'react';
-import { Button, Modal } from 'antd';
+import { Button, Modal, Form, Input, InputNumber, Select, DatePicker } from 'antd';
+import { TransactionType } from '../types/TransactionType';
+import dayjs from "dayjs";
+const { TextArea } = Input;
 
 export default function TransactionModal(){
-
+  const [form] = Form.useForm();
   const [open, setOpen] = useState(false);
   const [confirmLoading, setConfirmLoading] = useState(false);
 
   const showModal = () => {
+    form.resetFields();
     setOpen(true);
   };
 
   const handleOk = () => {
-    setConfirmLoading(true);
-    setTimeout(() => {
-      setOpen(false);
-      setConfirmLoading(false);
-    }, 2000);
+    form.submit();
+    // setTimeout(() => {
+    //   setOpen(false);
+    //   setConfirmLoading(false);
+    // }, 2000);
   };
 
   const handleCancel = () => {
     setOpen(false);
   };
+
+  function handleSubmit(values: any){
+    setConfirmLoading(true);
+    console.log('submitted')
+  }
+
+  function handleSubmitFailed(){
+    
+  }
+
+  const typeOptions = Object.entries(TransactionType).map(v => ({label: v[0], value: v[1]}));
+
   return (
     <>
-      <Button type="primary" onClick={showModal}>
+      <Button type="primary" className="bg-blue-600" onClick={showModal}>
         Add Transaction
       </Button>
       <Modal
@@ -32,7 +48,48 @@ export default function TransactionModal(){
         onOk={handleOk}
         confirmLoading={confirmLoading}
         onCancel={handleCancel}
+        okButtonProps={{className: "bg-blue-600"}}
+        okText="Save"
       >
+        <Form
+          form={form}
+          autoComplete='off'
+          autoCorrect='off'
+          layout='vertical'
+          initialValues={{type: typeOptions[0].value, date: dayjs()}}
+          onFinish={handleSubmit}
+          onFinishFailed={handleSubmitFailed}
+        >
+
+          <Form.Item name="description" label="Description" rules={[{required: true}]}>
+            <Input />
+          </Form.Item>
+
+          <Form.Item name="amount" label="Amount" rules={[{required: true}]}>
+            <InputNumber min="0.00" step="0.01" stringMode precision={2}/>
+          </Form.Item>
+
+          <Form.Item name="type" label="Type" rules={[{required: true}]}>
+            <Select options={typeOptions} />
+          </Form.Item>
+
+          <Form.Item name="date" label="Date" rules={[{required: true}]}>
+            <DatePicker />
+          </Form.Item>
+
+          <Form.Item name="category" label="Category" rules={[{required: true}]}>
+            <Select options={[]} />
+          </Form.Item>
+
+          <Form.Item name="tags" label="Tags">
+            <Select mode="multiple" options={[]} />
+          </Form.Item>
+
+          <Form.Item name="notes" label="Notes">
+            <TextArea />
+          </Form.Item>
+
+        </Form>
       </Modal>
     </>
   )
---
+++ src/client/types/TransactionType.ts
@@ -0,0 +1,4 @@
+export const TransactionType = {
+    Expense: 'expense',
+    Income: 'income'
+} as const;
